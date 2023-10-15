a537919eef3ff68fdb8a7c3aaecf897543b134dd
Markus Korn <markus.korn@gmail.com>
2023-10-15 20:32:11 +0200

    refactor(vec3.zig): remove unnecessary file write/read test
    This test was not related to the functionality of the vec3 module and was cluttering the test suite.

--- src/vec3.zig
+++ src/vec3.zig
@@ -134,18 +134,3 @@ test "vec3 is equal" {
 //    const v1 = Vec3.init(1.5, 2.0, 3.0);
 //    debug_vec3(v1);
 //}
-
-test {
-    const value = "hello";
-
-    var my_file = try std.fs.cwd().createFile("file.txt", .{ .read = true });
-    defer my_file.close();
-
-    _ = try my_file.write(value);
-
-    var buf: [value.len]u8 = undefined;
-    try my_file.seekTo(0);
-    const read = try my_file.read(&buf);
-
-    try std.testing.expectEqualStrings(value, buf[0..read]);
-}
