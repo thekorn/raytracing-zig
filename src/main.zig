f48f45b91df3b4dc3a74e3c34f006943c19a0c73
Markus Korn <markus.korn@gmail.com>
2023-10-15 10:43:54 +0200

    feat(main.zig): replace placeholder code with image generation logic
    The previous placeholder code was removed and replaced with logic to generate an image. The image dimensions are set to 256x256. The image is generated by iterating over each pixel and calculating the RGB values. The RGB values are then written to stdout. A progress message is also printed to stderr for each scanline processed.

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,6 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
@@ -11,14 +8,25 @@ pub fn main() !void {
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const image_height = 256;
+    const image_width = 256;
 
-    try bw.flush(); // don't forget to flush!
-}
+    try stdout.print("P3\n{d} {d}\n255\n", .{ image_height, image_width });
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    for (0..image_height) |j| {
+        std.debug.print("\rScanlines remaining: {d} ", .{image_height - j});
+        for (0..image_width) |i| {
+            const r = @as(f32, @floatFromInt(i)) / (image_width - 1);
+            const g = @as(f32, @floatFromInt(j)) / (image_height - 1);
+            const b = 0;
+            try stdout.print("{d} {d} {d}\n", .{
+                @as(usize, @intFromFloat(256 * r)),
+                @as(usize, @intFromFloat(256 * g)),
+                @as(usize, @intFromFloat(256 * b)),
+            });
+        }
+    }
+    std.debug.print("\rDone.                  \n", .{});
+
+    try bw.flush();
 }
