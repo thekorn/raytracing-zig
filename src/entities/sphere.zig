34bedca0a5dd683f7f75368077a2930a7bfa7945
Markus Korn <markus.korn@gmail.com>
2023-10-15 22:54:14 +0200

    refactor(sphere.zig): remove hit_sphere function as it's no longer needed
    The hit_sphere function was removed because its functionality has been incorporated into the Sphere struct, making the standalone function redundant.

--- src/entities/sphere.zig
+++ src/entities/sphere.zig
@@ -45,18 +45,3 @@ pub const Sphere = struct {
         return true;
     }
 };
-
-pub fn hit_sphere(center: Vec3, radius: f32, r: Ray) f32 {
-    const oc = r.origin.sub(center);
-    const a = r.direction.length_squared();
-    const half_b = oc.dot(r.direction);
-    const c = oc.length_squared() - radius * radius;
-
-    const discriminant = half_b * half_b - a * c;
-
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - @sqrt(discriminant)) / a;
-    }
-}
