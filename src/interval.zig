bac6c71bbd7bd26aabc9605f8aff6572cc787559
Markus Korn <markus.korn@gmail.com>
2023-11-19 14:10:02 +0100

    refactor(interval.zig, rtweekend.zig): replace getInfinity function with global Infinity constant feat(interval.zig): change min and max types from u32 to f64 to support negative and floating point values test(interval.zig, rtweekend.zig): add tests for default interval and global Infinity constant usage

--- src/interval.zig
+++ src/interval.zig
@@ -1,24 +1,32 @@
 const expect = @import("std").testing.expect;
 const rtweekend = @import("rtweekend.zig");
 
-const getInfinity = rtweekend.getInfinity;
+const Infinity = rtweekend.Infinity;
 
 pub const Interval = struct {
-    min: u32,
-    max: u32,
+    min: f64,
+    max: f64,
 
     const Self = @This();
 
-    pub fn init(min: u32, max: u32) Self {
+    pub fn init(min: f64, max: f64) Self {
         return .{ .min = min, .max = max };
     }
 
     pub fn defaultInit() Self {
-        return .{ .min = getInfinity(u32), .max = getInfinity(u32) };
+        return .{ .min = -Infinity, .max = Infinity };
     }
 };
 
 test "get interval [0, 10] interval" {
     const interval = Interval.init(0, 10);
     try expect(interval.min == 0);
+}
+
+test "get default interval" {
+    const interval = Interval.defaultInit();
+    try expect(interval.min == -Infinity);
+    try expect(interval.min < 0);
+    try expect(interval.max == Infinity);
+    try expect(interval.max > 0);
 }
