3a660cf4cc75348d5653f477f35b5dda7ea0aa30
Markus Korn <markus.korn@gmail.com>
2023-11-19 14:31:37 +0100

    Interval: new .contains() and .surrounds()

--- src/interval.zig
+++ src/interval.zig
@@ -16,11 +16,37 @@ pub const Interval = struct {
     pub fn defaultInit() Self {
         return .{ .min = -Infinity, .max = Infinity };
     }
+
+    pub fn contains(self: Self, value: f64) bool {
+        return self.min <= value and value <= self.max;
+    }
+
+    pub fn surrounds(self: Self, value: f64) bool {
+        return self.min < value and value < self.max;
+    }
 };
 
-test "get interval [0, 10] interval" {
+pub const Empty = Interval.init(Infinity, -Infinity);
+pub const Universe = Interval.init(-Infinity, Infinity);
+
+test "[0, 10] interval" {
     const interval = Interval.init(0, 10);
     try expect(interval.min == 0);
+}
+
+test "interval [0, 10] contains" {
+    const interval = Interval.init(0, 10);
+    try expect(interval.contains(5));
+    try expect(interval.contains(10));
+    try expect(!interval.contains(50));
+    try expect(!interval.contains(-50));
+}
+
+test "interval [0, 10] sourrounds" {
+    const interval = Interval.init(0, 10);
+    try expect(interval.surrounds(5));
+    try expect(!interval.surrounds(10));
+    try expect(!interval.surrounds(0));
 }
 
 test "get default interval" {
